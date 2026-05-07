60f521f9a9164d47b3bdbf57531f98a53775b64e
Christian Franzen <christian.franzen@rwth-aachen.de>
2026-05-08 00:14:28 +0200

    ci: Implemented debug support for service tests
    (cherry picked from commit 7e6fe8e548b47914b3e7540b31e146d220028d29)

--- testcontainers-floci/src/test/java/io/floci/testcontainers/AbstractFlociContainerServiceTest.java
+++ testcontainers-floci/src/test/java/io/floci/testcontainers/AbstractFlociContainerServiceTest.java
@@ -1,6 +1,8 @@
 package io.floci.testcontainers;
 
+import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.awscore.client.builder.AwsClientBuilder;
@@ -16,11 +18,22 @@ abstract class AbstractFlociContainerServiceTest {
 
     protected static final int LB_LISTENER_PORT = 8780;
 
-    protected static final FlociContainer floci = new FlociContainer()
-            .withLogLevel(Level.DEBUG)
-            .withElbV2Config(c -> c.listenerPort(LB_LISTENER_PORT));
+    private static final boolean DEBUG_LOGGING = false;
+
+    protected static final FlociContainer floci;
 
     static {
+        if (DEBUG_LOGGING) {
+            floci = new FlociContainer()
+                    .withLogLevel(Level.DEBUG)
+                    .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("DOCKER")))
+                    .withElbV2Config(c -> c.listenerPort(LB_LISTENER_PORT));
+        } else {
+            floci = new FlociContainer()
+                    .withLogLevel(Level.INFO)
+                    .withElbV2Config(c -> c.listenerPort(LB_LISTENER_PORT));
+        }
+
         floci.start();
 
         // Floci speaks JSON 1.1 — disable CBOR which is used by some service clients (e.g. Kinesis SDK) as default
